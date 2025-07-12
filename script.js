b5590c8630b6f4933d718f63d0dd0cece75a1b22
R-Htu <htuaung89@gmail.com>
2025-07-12 15:08:02 +0630

    moon sun added

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
 function toggleTheme() {
-        document.documentElement.classList.toggle('dark')
- }
+      const html = document.documentElement;
+      const icon = document.querySelector('#iconBtn i');
+      const isDark = html.classList.contains('dark');
+
+      // Toggle the dark class on <html>
+      html.classList.toggle('dark');
+
+      // Change the icon
+      if (isDark) {
+        icon.classList.remove('fa-sun');
+        icon.classList.add('fa-moon');
+      } else {
+        icon.classList.remove('fa-moon');
+        icon.classList.add('fa-sun');
+      }
+    }
