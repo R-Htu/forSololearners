a74e835d4e622ea4c206b93749c8f11b9b547d23
R-Htu <htuaung89@gmail.com>
2025-07-12 16:14:15 +0630

    plus box added

--- script.js
+++ script.js
@@ -3,10 +3,10 @@ function toggleTheme() {
       const icon = document.querySelector('#iconBtn i');
       const isDark = html.classList.contains('dark');
 
-      // Toggle the dark class on <html>
+      //	(ㆆ _ ㆆ) Toggle the dark class on <html>
       html.classList.toggle('dark');
 
-      // Change the icon
+      // ☜(⌒▽⌒)☞ Change the icon
       if (isDark) {
         icon.classList.remove('fa-sun');
         icon.classList.add('fa-moon');
@@ -15,3 +15,23 @@ function toggleTheme() {
         icon.classList.add('fa-sun');
       }
     }
+
+     const hLine = document.getElementById('hLine');
+    const vLine = document.getElementById('vLine');
+    const button = document.getElementById('togglePlus');
+
+    let isOpen = true;
+
+    button.addEventListener('click', () => {
+      if (isOpen) {
+        ///|\ ^._.^ /|\ Shrink lines
+        hLine.classList.replace('w-full', 'w-0');
+        vLine.classList.replace('h-full', 'h-0');
+      } else {
+        // ┌( ͝° ͜ʖ͡°)=ε/̵͇̿̿/’̿’̿ ̿   Expand lines
+        hLine.classList.replace('w-0', 'w-full');
+        vLine.classList.replace('h-0', 'h-full');
+      }
+      button.setAttribute('aria-pressed', !isOpen);
+      isOpen = !isOpen;
+    });
